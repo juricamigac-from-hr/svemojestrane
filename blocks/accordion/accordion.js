b237423ec8e1155c87162b545c0dc024c0d45f6f
Jurica Migač <jurica1.migac@gmail.com>
2026-04-05 20:08:44 +0200

    Accordion changes

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,9 +1,3 @@
-/*
- * Accordion Block
- * Recreate an accordion
- * https://www.hlx.live/developer/block-collection/accordion
- */
-
 export default function decorate(block) {
   [...block.children].forEach((row) => {
     // decorate accordion item label
